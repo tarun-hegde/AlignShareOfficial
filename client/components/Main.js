2f008446b0d509ab7903a27b6bb10ff577dee965
Royal Simpson Pinto <royalpinto007@gmail.com>
2024-05-25 14:29:28 +0530

    fix: steps and titles
    Signed-off-by: Royal Simpson Pinto <royalpinto007@gmail.com>

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,34 +1,39 @@
-'use client'
+"use client";
 
 export default function Main() {
   return (
     <section className="text-black body-font lg:pt-20">
       <div className="container px-5 pt-32 mx-auto lg:px-4 lg:py-4">
         <div className="flex flex-col w-full mb-2 text-left md:text-center ">
-          
           <h2 className="mb-8 text-4xl font-bold tracking-tighter text-white lg:text-6xl md:text-5xl animate-jump-in animate-once animate-delay-800 animate-ease-linear animate-fill-both">
             <span>Effortless communication</span>
             <br className="hidden lg:block"></br>
           </h2>
           <br></br>
-          <div className="flex justify-center items-center animate-bounce animate-thrice animate-ease-in-out animate-normal animate-delay-1000"> 
-            <div className= "pt-4">
-              <p className="text-lg mb-4 text-white mr-4 ">Want to get started?</p>
+          <div className="flex justify-center items-center animate-bounce animate-thrice animate-ease-in-out animate-normal animate-delay-1000">
+            <div className="pt-4">
+              <p className="text-lg mb-4 text-white mr-4 ">Need help?</p>
             </div>
-            <button 
-              style={{ 
-                backgroundColor: 'white', 
-                color: 'black', 
-                padding: "10px 10px", 
-                border: "2px solid black", 
-                fontSize: "12px", 
-                fontWeight: "bold", 
-                borderRadius: "10px", 
+            <button
+              style={{
+                backgroundColor: "white",
+                color: "black",
+                padding: "10px 10px",
+                border: "2px solid black",
+                fontSize: "12px",
+                fontWeight: "bold",
+                borderRadius: "10px",
                 transition: "background-color 0.3s ease",
-                cursor: "pointer"
+                cursor: "pointer",
               }}
-              onMouseEnter={(e) => { e.target.style.backgroundColor = 'grey'; e.target.style.color = 'white'; }}
-              onMouseLeave={(e) => { e.target.style.backgroundColor = 'white'; e.target.style.color = 'black'; }}
+              onMouseEnter={(e) => {
+                e.target.style.backgroundColor = "grey";
+                e.target.style.color = "white";
+              }}
+              onMouseLeave={(e) => {
+                e.target.style.backgroundColor = "white";
+                e.target.style.color = "black";
+              }}
             >
               Click here
             </button>
--- client/components/ui/steps.jsx
+++ client/components/ui/steps.jsx
@@ -1,78 +1,85 @@
-import React from 'react';
-
+import React from "react";
 
 const Steps = () => {
   return (
-    <div className="w-full px-4 text-white text-gray-900 bg-black">
-      <div className="tree-wrapper relative md:w-[10px] min-h-[50vh] md:mx-auto">
-        <div className="flex md:flex-row flex-col md:items-center md:gap-2 group relative top-0 md:left-1/2 md:right-1/2 md:even:translate-x-[-100%] even:translate-x-0 md:w-max w-full z-[1]">
-          <span className="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -start-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
-            <svg className="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-            </svg>
-          </span>
-          <div className="details md:max-w-sm p-5 m-10 bg-gray-100 dark:bg-gray-900 rounded-md my-3">
-            <div className="course-title">B.Sc.</div>
-            <div className="institution">Feni Govt College</div>
-            <div className="location">Feni, Bangladesh</div>
-            <div className="starting-year">2015</div>
-            <div className="graduation-year">2017</div>
-            <div className="description">its a short journey of my educational life in the Feni Gov't college and i tried to use time best use of time in this short period</div>
-            <div className="result">GPA 4.58</div>
+    <div className="container bg-black mx-auto w-full">
+      <div className="flex justify-center items-center">
+        <h2 className="mb-8 text-4xl font-bold tracking-tighter text-blue-600 lg:text-6xl md:text-5xl animate-jump-in animate-once animate-delay-800 animate-ease-linear animate-fill-both text-center">
+          <span>How to use?</span>
+          <br className="hidden lg:block"></br>
+        </h2>
+      </div>
+      <div className="relative wrap overflow-hidden p-10 h-full">
+        <div
+          className="border-2-2 absolute border-opacity-20 border-white h-full border"
+          style={{ left: "50%" }}
+        ></div>
+
+        <div className="mb-8 flex justify-between items-center w-full right-timeline">
+          <div className="order-1 w-5/12"></div>
+          <div className="z-20 flex items-center order-1 bg-blue-600 shadow-xl w-8 h-8 rounded-full">
+            <h1 className="mx-auto font-semibold text-lg text-white">1</h1>
+          </div>
+          <div className="order-1 bg-white rounded-lg shadow-xl w-5/12 px-6 py-4 animate-wiggle animate-thrice animate-duration-[5000ms] animate-ease-linear">
+            <h3 className="mb-3 font-bold text-gray-800 text-xl">
+              Create an account
+            </h3>
+            <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
           </div>
         </div>
 
-
-        <div className="flex md:flex-row flex-col md:items-center md:gap-2 group relative top-0 md:left-1/2 md:right-1/2 md:even:translate-x-[-100%] even:translate-x-0 md:w-max w-full z-[1]">
-          <span className="absolute  ml-[465px] flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -start-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
-            <svg className="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-            </svg>
-          </span>
-          <div className="details md:max-w-sm p-5 m-10 bg-gray-100 dark:bg-gray-900 rounded-md my-3">
-            <div className="course-title">B.Sc.</div>
-            <div className="institution">Feni Govt College</div>
-            <div className="location">Feni, Bangladesh</div>
-            <div className="starting-year">2015</div>
-            <div className="graduation-year">2017</div>
-            <div className="description">its a short journey of my educational life in the Feni Gov't college and i tried to use time best use of time in this short period</div>
-            <div className="result">GPA 4.58</div>
+        <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+          <div className="order-1 w-5/12"></div>
+          <div className="z-20 flex items-center order-1 bg-blue-600 shadow-xl w-8 h-8 rounded-full">
+            <h1 className="mx-auto text-white font-semibold text-lg">2</h1>
+          </div>
+          <div className="order-1 bg-blue-600 rounded-lg shadow-xl w-5/12 px-6 py-4 animate-wiggle animate-thrice animate-duration-[5000ms] animate-ease-linear">
+            <h3 className="mb-3 font-bold text-white text-xl">Fetch your company updates</h3>
+            <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
           </div>
         </div>
 
-
-        <div className="flex md:flex-row flex-col md:items-center md:gap-2 group relative top-0 md:left-1/2 md:right-1/2 md:even:translate-x-[-100%] even:translate-x-0 md:w-max w-full z-[1]">
-          <span className="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -start-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
-            <svg className="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-            </svg>
-          </span>
-          <div className="details md:max-w-sm p-5 m-10 bg-gray-100 dark:bg-gray-900 rounded-md my-3">
-            <div className="course-title">B.Sc.</div>
-            <div className="institution">Feni Govt College</div>
-            <div className="location">Feni, Bangladesh</div>
-            <div className="starting-year">2015</div>
-            <div className="graduation-year">2017</div>
-            <div className="description">its a short journey of my educational life in the Feni Gov't college and i tried to use time best use of time in this short period</div>
-            <div className="result">GPA 4.58</div>
+        <div className="mb-8 flex justify-between items-center w-full right-timeline">
+          <div className="order-1 w-5/12"></div>
+          <div className="z-20 flex items-center order-1 bg-blue-600 shadow-xl w-8 h-8 rounded-full">
+            <h1 className="mx-auto font-semibold text-lg text-white">3</h1>
+          </div>
+          <div className="order-1 bg-white rounded-lg shadow-xl w-5/12 px-6 py-4 animate-wiggle animate-thrice animate-duration-[5000ms] animate-ease-linear">
+            <h3 className="mb-3 font-bold text-gray-800 text-xl">
+              Generate content for social media
+            </h3>
+            <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
           </div>
         </div>
 
-
-        <div className="flex md:flex-row flex-col md:items-center md:gap-2 group relative top-0 md:left-1/2 md:right-1/2 md:even:translate-x-[-100%] even:translate-x-0 md:w-max w-full z-[1]">
-          <span className="absolute ml-[465px] flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -start-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
-            <svg className="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-            </svg>
-          </span>
-          <div className="details md:max-w-sm p-5 m-10 bg-gray-100 dark:bg-gray-900 rounded-md my-3">
-            <div className="course-title">B.Sc.</div>
-            <div className="institution">Feni Govt College</div>
-            <div className="location">Feni, Bangladesh</div>
-            <div className="starting-year">2015</div>
-            <div className="graduation-year">2017</div>
-            <div className="description">its a short journey of my educational life in the Feni Gov't college and i tried to use time best use of time in this short period</div>
-            <div className="result">GPA 4.58</div>
+        <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+          <div className="order-1 w-5/12"></div>
+          <div className="z-20 flex items-center order-1 bg-blue-600 shadow-xl w-8 h-8 rounded-full">
+            <h1 className="mx-auto text-white font-semibold text-lg">4</h1>
+          </div>
+          <div className="order-1 bg-blue-600 rounded-lg shadow-xl w-5/12 px-6 py-4 animate-wiggle animate-thrice animate-duration-[5000ms] animate-ease-linear">
+            <h3 className="mb-3 font-bold text-white text-xl">Direct option to post</h3>
+            <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
           </div>
         </div>
       </div>
